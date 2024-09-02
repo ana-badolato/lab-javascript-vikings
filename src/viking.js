9bb97906d90f134d62c5eaeed2cf480a7c1219d2
Ana Badolato <badolatoana@gmail.com>
2024-09-02 15:32:44 +0200

    Iteration 4

--- src/viking.js
+++ src/viking.js
@@ -52,4 +52,50 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+
+  addViking(newVicking) {
+    this.vikingArmy.push(newVicking);
+  }
+
+  addSaxon(newSaxon) {
+    this.saxonArmy.push(newSaxon);
+  }
+
+  vikingAttack() {
+    let vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+    let saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let saxon = this.saxonArmy[saxonRandom];
+    let viking = this.vikingArmy[vikingRandom];
+
+    let result = saxon.receiveDamage(viking.attack());
+
+    if (saxon.health <= 0) {
+        this.saxonArmy.splice(saxonRandom, 1);
+    }
+
+    return result;
+}
+
+  saxonAttack() {
+    let vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+    let saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+
+    let saxon = this.saxonArmy[saxonRandom];
+    let viking = this.vikingArmy[vikingRandom];
+
+    let result = viking.receiveDamage(saxon.attack());
+
+    if (viking.health <= 0) {
+        this.vikingArmy.splice(vikingRandom, 1);
+    }
+
+    return result;
+}
+}
+
